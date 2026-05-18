f1113b80da0fcef779d594604c0849462ce8a8ff
Sudharani Inti <inti.sudharani@ignitarium.com>
2026-05-18 05:39:55 +0000

    Isolated 18 low-pcc failing tests for TANH activation

---
+++ tests/ttnn/nightly/unit_tests/operations/matmul/test_sudha_matmul_tanh_activation.py
@@ -0,0 +1,559 @@
+# SPDX-FileCopyrightText: © 2026 Tenstorrent USA, Inc.
+
+# SPDX-License-Identifier: Apache-2.0
+
+import pytest
+from loguru import logger
+import ttnn
+from models.common.utility_functions import is_wormhole_b0, is_blackhole
+from models.common.utility_functions import torch2tt_tensor, tt2torch_tensor, pad_by_zero, roundup32
+import torch
+import itertools
+from tests.ttnn.utils_for_testing import assert_numeric_metrics
+import random
+import math
+from tracy import signpost
+
+from models.demos.llama3_70b_galaxy.tt.model_config import (
+    PREFETCHER_NOC1_GRID,
+)
+
+
+random.seed(10)
+
+
+def num_cores_to_rectangle_grid(num_cores, device):
+    """
+    Find a rectangular core grid size, given an number of cores.
+
+    Return None if rectangle grid is not possible.
+    """
+    x = device.compute_with_storage_grid_size().x
+    while x > 0 and num_cores % x != 0:
+        x -= 1
+
+    if x == 0:
+        return None
+
+    y = num_cores // x
+    return (x, y)
+
+
+def get_physical_to_logical_core_mapping(device):
+    """
+    Get a mapping from physical core coords to logical core coords
+
+    Returns a dictionary.
+    """
+    mapping = {}
+    grid = device.compute_with_storage_grid_size()
+    for x in range(grid.x):
+        for y in range(grid.y):
+            physical_core = device.worker_core_from_logical_core(ttnn.CoreCoord(x, y))
+            mapping[(physical_core.x, physical_core.y)] = (x, y)
+    return mapping
+
+
+def round_up(a, b):
+    """
+    Round up a to the nearest multiple of b
+    """
+    return b * math.ceil(a / b)
+
+
+# physical coords
+PREFETCHER_GRID = [
+    (8, 11),
+    (8, 9),
+    (8, 8),
+    (8, 7),
+    (8, 5),
+    (8, 3),
+    (8, 2),
+    (8, 1),
+    (7, 1),
+    (7, 2),
+    (7, 3),
+    (7, 5),
+    (7, 7),
+    (7, 8),
+    (7, 9),
+    (7, 11),
+    (3, 11),
+    (3, 7),
+    (3, 5),
+    (3, 1),
+    (2, 1),
+    (2, 5),
+    (2, 7),
+    (2, 11),
+]
+
+# dram sharded MM output logical coords
+PREFETCHER_NOC1_OUTPUT_GRID = [
+    (1, 9),
+    (2, 9),
+    (1, 0),
+    (2, 0),
+    (1, 4),
+    (2, 4),
+    (1, 5),
+    (2, 5),
+    (5, 0),
+    (6, 0),
+    (5, 9),
+    (6, 9),
+    (5, 1),
+    (6, 1),
+    (5, 7),
+    (6, 7),
+    (5, 6),
+    (6, 6),
+    (5, 2),
+    (6, 2),
+    (5, 4),
+    (6, 4),
+    (5, 5),
+    (6, 5),
+]
+
+LM_HEAD_32_GRID = list(
+    itertools.chain(
+        itertools.product([1, 2, 3], range(10)),  # Generates (1,0)-(1,9), (2,0)-(2,9), (3,0)-(3,9)
+        itertools.product([5], range(2)),  # Generates (5,0), (5,1)
+    )
+)
+
+
+def run_multi_core_matmul_1d(
+    device,
+    in0_dtype,
+    in1_dtype,
+    fidelity,
+    has_bias,
+    fp32_acc_mode,
+    packer_l1_acc,
+    B,
+    M,
+    K,
+    N,
+    activation,
+    grid,
+    use_arbitrary_cores,
+    num_iters,
+    output_dtype=None,
+    max_dst_tiles=8,
+    pcc_threshold=0.98,
+    use_physical_to_logical_mapping=True,
+    hop_grid=None,
+    in1_is_dram_interleaved=False,
+    in1_is_in_dram=False,
+    untilize_out=False,
+):
+    assert not has_bias, "Bias not supported for gather_in0 mode."
+    if not isinstance(grid, tuple) and not use_arbitrary_cores:
+        pytest.skip("Grid is not a tuple and not using arbitrary cores")
+
+    if output_dtype is None:
+        output_dtype = in0_dtype
+
+    in0_shape = [1, B, M, K]
+    in1_shape = [1, 1, K, N]
+    num_cores = grid[0] * grid[1] if isinstance(grid, tuple) else len(grid)
+
+    storage_grid = num_cores_to_rectangle_grid(num_cores, device)
+    if storage_grid is None:
+        pytest.skip(f"Could not find a rectangle grid for num_cores: {num_cores}")
+
+    M *= B  # Fuse batch always enabled
+
+    K_per_shard = round_up(math.ceil(K / num_cores), ttnn.TILE_SIZE)
+    K_padded = K_per_shard * num_cores
+    N_per_shard = round_up(math.ceil(N / num_cores), ttnn.TILE_SIZE)
+    N_per_shard_in_dram = N_per_shard * 2
+    N_padded = N_per_shard * num_cores
+
+    logger.info(f"K_per_shard {K_per_shard}")
+    logger.info(f"K_padded {K_padded}")
+    logger.info(f"N_per_shard {N_per_shard}")
+    logger.info(f"N_padded {N_padded}")
+
+    in0_block_h = M // ttnn.TILE_SIZE
+    in0_block_w = K // num_cores // ttnn.TILE_SIZE
+    while (K / ttnn.TILE_SIZE) % in0_block_w != 0:
+        in0_block_w -= 1
+
+    out_block_h = M // ttnn.TILE_SIZE
+    out_block_w = N_padded // num_cores // ttnn.TILE_SIZE
+
+    num_blocks_y = (M // ttnn.TILE_SIZE - 1) // out_block_h + 1
+    num_blocks_x = (N_padded // ttnn.TILE_SIZE - 1) // out_block_w + 1
+    num_blocks_total = num_blocks_y * num_blocks_x
+
+    if num_blocks_total != num_cores:
+        pytest.skip(f"num_blocks_total {num_blocks_total} != num_cores {num_cores}")
+
+    out_subblock_h = 1
+    out_subblock_w = max_dst_tiles
+    while out_block_w % out_subblock_w != 0:
+        out_subblock_w -= 1
+
+    logger.debug("in0 block h w " + str(in0_block_h) + " " + str(in0_block_w))
+    logger.debug("in1 block h w " + str(in0_block_w) + " " + str(out_block_w))
+    logger.debug("out block h w " + str(out_block_h) + " " + str(out_block_w))
+    logger.debug("out subblock h w " + str(out_subblock_h) + " " + str(out_subblock_w))
+
+    if use_arbitrary_cores:
+        # x, y
+        if isinstance(grid, tuple):  # Generate random grid
+            CORE_RANGE = [(x, y) for y in range(storage_grid[1]) for x in range(storage_grid[0])]
+            random.shuffle(CORE_RANGE)
+        else:  # Use custom grid
+            if use_physical_to_logical_mapping:
+                mapping = get_physical_to_logical_core_mapping(device)
+                CORE_RANGE = [mapping[physical_coord] for physical_coord in grid]
+            else:
+                CORE_RANGE = grid
+
+        core_range_set = ttnn.CoreRangeSet(
+            [
+                ttnn.CoreRange(
+                    ttnn.CoreCoord(x, y),
+                    ttnn.CoreCoord(x, y),
+                )
+                for x, y in CORE_RANGE
+            ]
+        )
+    else:
+        core_range_set = ttnn.CoreRangeSet(
+            {
+                ttnn.CoreRange(
+                    ttnn.CoreCoord(0, 0),
+                    ttnn.CoreCoord(storage_grid[0] - 1, storage_grid[1] - 1),
+                ),
+            }
+        )
+
+    hop_core_range_set = ttnn.CoreRangeSet([])
+    if hop_grid is not None:
+        hop_core_range_set = ttnn.CoreRangeSet(
+            {
+                ttnn.CoreRange(
+                    ttnn.CoreCoord(x, y),
+                    ttnn.CoreCoord(x, y),
+                )
+                for x, y in hop_grid
+            }
+        )
+
+    in0_sharded_mem_config = ttnn.MemoryConfig(
+        ttnn.TensorMemoryLayout.WIDTH_SHARDED,
+        ttnn.BufferType.L1,
+        ttnn.ShardSpec(
+            core_range_set,
+            [M, K_per_shard],
+            ttnn.ShardOrientation.ROW_MAJOR,
+        ),
+    )
+
+    if in1_is_in_dram:
+        if in1_is_dram_interleaved:
+            in1_sharded_mem_config = ttnn.DRAM_MEMORY_CONFIG
+        else:
+            in1_shard_grid = ttnn.CoreCoord(device.dram_grid_size().x - 1, device.dram_grid_size().y - 1)
+            in1_shard_grid = ttnn.CoreRangeSet({ttnn.CoreRange(ttnn.CoreCoord(0, 0), in1_shard_grid)})
+            in1_sharded_mem_config = ttnn.MemoryConfig(
+                ttnn.TensorMemoryLayout.WIDTH_SHARDED,
+                ttnn.BufferType.DRAM,
+                ttnn.ShardSpec(
+                    in1_shard_grid,
+                    [K, N_per_shard_in_dram],
+                    ttnn.ShardOrientation.ROW_MAJOR,
+                ),
+            )
+    else:
+        in1_sharded_mem_config = ttnn.MemoryConfig(
+            ttnn.TensorMemoryLayout.WIDTH_SHARDED,
+            ttnn.BufferType.L1,
+            ttnn.ShardSpec(
+                core_range_set,
+                [K, N_per_shard],
+                ttnn.ShardOrientation.ROW_MAJOR,
+            ),
+        )
+
+    dram_sharded_output_core_range_set = ttnn.CoreRangeSet(
+        [
+            ttnn.CoreRange(
+                ttnn.CoreCoord(x, y),
+                ttnn.CoreCoord(x, y),
+            )
+            for x, y in PREFETCHER_NOC1_OUTPUT_GRID
+        ]
+    )
+
+    output_sharded_mem_config = ttnn.MemoryConfig(
+        ttnn.TensorMemoryLayout.WIDTH_SHARDED,
+        ttnn.BufferType.L1,
+        ttnn.ShardSpec(
+            dram_sharded_output_core_range_set if (in1_is_in_dram and not in1_is_dram_interleaved) else core_range_set,
+            [M, N_per_shard],
+            ttnn.ShardOrientation.ROW_MAJOR,
+        ),
+    )
+
+    in0 = torch.randn(in0_shape)
+    in1 = torch.randn(in1_shape)
+
+    in0_t = ttnn.from_torch(
+        in0,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        dtype=in0_dtype,
+        memory_config=in0_sharded_mem_config,
+    )
+    in1_t = ttnn.from_torch(
+        in1,
+        device=device,
+        layout=ttnn.TILE_LAYOUT,
+        dtype=in1_dtype,
+        memory_config=in1_sharded_mem_config,
+    )
+
+    program_config = ttnn.MatmulMultiCoreReuseMultiCast1DProgramConfig(
+        compute_with_storage_grid_size=storage_grid,
+        in0_block_w=in0_block_w,
+        out_subblock_h=out_subblock_h,
+        out_subblock_w=out_subblock_w,
+        per_core_M=out_block_h,
+        per_core_N=out_block_w,
+        fuse_batch=True,
+        fused_activation=activation,
+        mcast_in0=False,
+        gather_in0=True,
+        hop_cores=hop_core_range_set,
+        untilize_out=untilize_out,
+    )
+
+    compute_kernel_config = ttnn.init_device_compute_kernel_config(
+        device.arch(),
+        math_fidelity=fidelity,
+        math_approx_mode=True,
+        fp32_dest_acc_en=fp32_acc_mode,
+        packer_l1_acc=packer_l1_acc,
+        dst_full_sync_en=True,
+    )
+
+    signpost("start")
+    with device.cache_entries_counter.measure():
+        for _ in range(num_iters):
+            output_t = ttnn.matmul(
+                in0_t,
+                in1_t,
+                program_config=program_config,
+                memory_config=output_sharded_mem_config,
+                compute_kernel_config=compute_kernel_config,
+                dtype=output_dtype,
+            )
+    signpost("stop")
+    tt_out = ttnn.to_torch(output_t)
+    pt_out = in0 @ in1
+
+    if activation:
+        # Map ttnn activation types to PyTorch functions
+        if activation == ttnn.UnaryOpType.SILU:
+            pt_out = torch.nn.functional.silu(pt_out)
+        elif activation == ttnn.UnaryOpType.RELU:
+            pt_out = torch.nn.functional.relu(pt_out)
+        elif activation == ttnn.UnaryOpType.GELU:
+            pt_out = torch.nn.functional.gelu(pt_out)
+        elif activation == ttnn.UnaryOpType.TANH:
+            pt_out = torch.tanh(pt_out)
+        else:
+            raise ValueError(f"Unsupported activation type: {activation}")
+
+    if in0_dtype == ttnn.bfloat4_b or in1_dtype == ttnn.bfloat4_b or output_dtype == ttnn.bfloat4_b:
+        if activation == ttnn.UnaryOpType.TANH and packer_l1_acc and fp32_acc_mode:
+            # See issue #42856
+            # pcc_threshold = 0.913
+            pcc_threshold = 0.99
+        else:
+            pcc_threshold = 0.99
+
+        assert_numeric_metrics(
+            pt_out,
+            tt_out,
+            atol=0.049 * K,
+            rtol=39.159 * K,
+            frobenius_threshold=0.002 * K,
+            pcc_threshold=pcc_threshold,
+            check_ulp=False,
+        )
+    elif in0_dtype == ttnn.bfloat8_b or in1_dtype == ttnn.bfloat8_b or output_dtype == ttnn.bfloat8_b:
+        assert_numeric_metrics(
+            pt_out,
+            tt_out,
+            atol=0.013 * K,
+            rtol=9.439 * K,
+            frobenius_threshold=0.001 * K,
+            pcc_threshold=0.99,
+            check_ulp=False,
+        )
+    else:
+        assert_numeric_metrics(
+            pt_out, tt_out, check_allclose=False, check_frobenius=False, pcc_threshold=0.99, check_ulp=False
+        )
+
+    # Check program cache
+    assert device.cache_entries_counter.total == 1  # Only 1 op
+
+
+@pytest.mark.skipif(is_blackhole(), reason="Test suite for WH only")
+@pytest.mark.parametrize("has_bias", [False], ids=["no_bias"])
+@pytest.mark.parametrize(
+    "B, M, K, N, in0_dtype, in1_dtype, fidelity, packer_l1_acc, fp32_acc_mode, grid",
+    [
+        # 32, 2304, 3840
+        (1, 32, 2304, 3840, ttnn.bfloat16, ttnn.bfloat4_b, ttnn.MathFidelity.LoFi, True, True, (8, 3)),
+        # 32, 2304, 3840
+        (3, 32, 2304, 3840, ttnn.bfloat16, ttnn.bfloat4_b, ttnn.MathFidelity.LoFi, True, True, (8, 3)),
+        # 256, 1024, 8192
+        (1, 256, 1024, 8192, ttnn.bfloat16, ttnn.bfloat4_b, ttnn.MathFidelity.HiFi4, True, True, (8, 4)),
+        # 256, 1024, 8192
+        (1, 256, 1024, 8192, ttnn.bfloat16, ttnn.bfloat4_b, ttnn.MathFidelity.HiFi4, True, True, (8, 4)),
+    ],
+)
+@pytest.mark.parametrize(
+    "activation",
+    [
+        #     None,
+        #     ttnn.UnaryOpType.SILU,
+        #     ttnn.UnaryOpType.RELU,
+        #     ttnn.UnaryOpType.GELU,
+        ttnn.UnaryOpType.TANH,
+    ],
+)
+@pytest.mark.parametrize(
+    "use_arbitrary_cores",
+    [False, True],
+)
+@pytest.mark.parametrize(
+    "num_iters",
+    [1, 3],
+)
+def test_multi_core_matmul_1d_wh(
+    device,
+    in0_dtype,
+    in1_dtype,
+    fidelity,
+    has_bias,
+    fp32_acc_mode,
+    packer_l1_acc,
+    B,
+    M,
+    K,
+    N,
+    activation,
+    grid,
+    use_arbitrary_cores,
+    num_iters,
+    function_level_defaults,
+):
+    torch.manual_seed(0)
+    run_multi_core_matmul_1d(
+        device,
+        in0_dtype,
+        in1_dtype,
+        fidelity,
+        has_bias,
+        fp32_acc_mode,
+        packer_l1_acc,
+        B,
+        M,
+        K,
+        N,
+        activation,
+        grid,
+        use_arbitrary_cores,
+        num_iters,
+    )
+
+
+@pytest.mark.skipif(is_blackhole(), reason="Test suite for WH only")
+@pytest.mark.parametrize("has_bias", [False], ids=["no_bias"])
+@pytest.mark.parametrize(
+    "B, M, K, N, in0_dtype, in1_dtype, fidelity, packer_l1_acc, fp32_acc_mode, grid",
+    [
+        # Disabled for post-commit
+        # (1, 32, 2304, 3840, ttnn.bfloat16, ttnn.bfloat4_b, ttnn.MathFidelity.LoFi, True, True, PREFETCHER_NOC1_GRID),
+        (1, 32, 2304, 3840, ttnn.bfloat16, ttnn.bfloat4_b, ttnn.MathFidelity.LoFi, True, True, (8, 3)),
+    ],
+)
+@pytest.mark.parametrize(
+    "hop_grid",
+    [
+        # [
+        #     (3, 6),
+        # ],
+        [
+            (6, 3),
+        ],
+    ],
+)
+@pytest.mark.parametrize(
+    "activation",
+    [
+        # None,
+        # ttnn.UnaryOpType.SILU,
+        # ttnn.UnaryOpType.RELU,
+        # ttnn.UnaryOpType.GELU,
+        ttnn.UnaryOpType.TANH,
+    ],
+)
+@pytest.mark.parametrize(
+    "use_arbitrary_cores",
+    [False],
+)
+@pytest.mark.parametrize(
+    "num_iters",
+    [1, 3],
+)
+def test_multi_core_matmul_1d_ring_hop_wh(
+    device,
+    in0_dtype,
+    in1_dtype,
+    fidelity,
+    has_bias,
+    fp32_acc_mode,
+    packer_l1_acc,
+    B,
+    M,
+    K,
+    N,
+    activation,
+    grid,
+    hop_grid,
+    use_arbitrary_cores,
+    num_iters,
+    function_level_defaults,
+):
+    torch.manual_seed(0)
+    run_multi_core_matmul_1d(
+        device,
+        in0_dtype,
+        in1_dtype,
+        fidelity,
+        has_bias,
+        fp32_acc_mode,
+        packer_l1_acc,
+        B,
+        M,
+        K,
+        N,
+        activation,
+        grid,
+        use_arbitrary_cores,
+        num_iters,
+        use_physical_to_logical_mapping=False,
+        hop_grid=hop_grid,
+    )
